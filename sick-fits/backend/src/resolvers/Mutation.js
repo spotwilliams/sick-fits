ba0d18abe2c8dcb800fe479f26ee225ca8eed344
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-09-07 20:34:21 -0300

    On memory mutations and querys

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, context, info) {
+        global.dogs = global.dogs || [];
+        const dog = {name: args.name};
+        global.dogs.push(dog);
+
+        return dog;
+    }
+};
 
 module.exports = Mutations;
